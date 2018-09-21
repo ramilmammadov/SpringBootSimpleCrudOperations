d536b988fc6fbdca15ff155ebcc8500d4551d094
Ramil Mammadov <ramilmammadov95@gmail.com>
2018-09-21 10:48:08 +0400

    Spring Boot simple CRUD operations

---
+++ src/script.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+
+LOCK TABLES `user` WRITE;
+INSERT INTO `user` VALUES (1,'test','test@gmail.com');
+UNLOCK TABLES;
